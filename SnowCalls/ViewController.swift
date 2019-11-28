e631620a74c945e62a2cdf3208e81deae4a877ad
Chen Sihan <schen@lcs.on.ca>
2019-11-28 14:53:20 -0500

    conditions and using counts to add dashes

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -47,6 +47,7 @@ class ViewController: UIViewController {
         let textforNine = "WXYZwxyz"
         var convertedMessage = ""
         
+        
         for piece in textPieces {
             
             print(piece)
@@ -54,26 +55,69 @@ class ViewController: UIViewController {
             for character in piece {
                 
                 let characterAsString = String(character)
-                switch characterAsString {
-                case textforTwo.contains(characterAsString):
-                    convertedMessage += "2"
-                case textforThree:
-                    convertedMessage += "3"
-                case textforFour:
-                    convertedMessage += "4"
-                case textforFive:
-                    convertedMessage += "5"
-                case textforSix:
-                    convertedMessage += "6"
-                case textforSeven:
-                    convertedMessage += "7"
-                case textforEight:
-                    convertedMessage += "8"
-                case textforNine:
-                    convertedMessage += "9"
-                default:
+                
+                if convertedMessage.count == 11 {
                     convertedMessage += ""
                 }
+                else if convertedMessage.count == 3 {
+                    convertedMessage += "-"
+                }
+                else  if convertedMessage.count == 7 {
+                    convertedMessage += "-"
+                }
+                else if textforTwo.contains(characterAsString){
+                    convertedMessage += "2"
+                }
+                else if textforThree.contains(characterAsString){
+                    convertedMessage += "3"
+                }
+                else if textforFour.contains(characterAsString){
+                    convertedMessage += "4"
+                }
+                else if textforFive.contains(characterAsString){
+                    convertedMessage += "5"
+                }
+                else if textforSix.contains(characterAsString){
+                    convertedMessage += "6"
+                }
+                else if textforSeven.contains(characterAsString){
+                    convertedMessage += "7"
+                }
+                else if textforEight.contains(characterAsString){
+                    convertedMessage += "8"
+                }
+                else if textforNine.contains(characterAsString){
+                    convertedMessage += "9"
+                }
+                else {
+                    convertedMessage += ""
+                }
+                
+                
+                
+                
+                
+                
+                //                switch characterAsString {
+                //                case textforTwo.contains(characterAsString):
+                //                    convertedMessage += "2"
+                //                case textforThree:
+                //                    convertedMessage += "3"
+                //                case textforFour:
+                //                    convertedMessage += "4"
+                //                case textforFive:
+                //                    convertedMessage += "5"
+                //                case textforSix:
+                //                    convertedMessage += "6"
+                //                case textforSeven:
+                //                    convertedMessage += "7"
+                //                case textforEight:
+                //                    convertedMessage += "8"
+                //                case textforNine:
+                //                    convertedMessage += "9"
+                //                default:
+                //                    convertedMessage += ""
+                //                }
                 
             }
             
@@ -81,9 +125,11 @@ class ViewController: UIViewController {
             
             
             
-            
-            
         }
+        
+        
+        
+        
         outputText.text = convertedMessage
     }
     
@@ -92,4 +138,5 @@ class ViewController: UIViewController {
     
     
 }
+
 
