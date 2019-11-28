62a96e3aca843170f74ac3ba23722acc86b812ea
Chen Sihan <schen@lcs.on.ca>
2019-11-27 19:49:09 -0500

    Initial Commit

---
+++ SnowCalls/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SnowCalls
+//
+//  Created by Chen, Sihan on 2019-11-27.
+//  Copyright © 2019 Chen, Sihan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
