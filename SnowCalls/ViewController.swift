b94f7470e9d9d8c8df6a5fd651742cadec9fb4e8
Chen Sihan <schen@lcs.on.ca>
2019-11-28 16:45:15 -0500

    Adding comments

--- SnowCalls/ViewController.swift
+++ SnowCalls/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    
+    // MARK: Prperties
     @IBOutlet weak var inputText: UITextView!
     
     
@@ -31,11 +31,13 @@ class ViewController: UIViewController {
     
     @IBAction func convertNumbers(_ sender: Any) {
         
+        // Make sure the text view is not empty
         guard let inputTextAsString = inputText.text, inputText.text != "" else {
             outputText.text = "Please enter phone numbers to convert."
             return
         }
         
+        // Used for converting, see if it contains the characters in the input
         let textPieces = inputTextAsString.split(separator: "\n")
         let textforTwo = "ABC"
         let textforThree = "DEF"
@@ -45,29 +47,34 @@ class ViewController: UIViewController {
         let textforSeven = "PQRS"
         let textforEight = "TUV"
         let textforNine = "WXYZ"
+        
+        // Run multiple times, check for output
         var convertedMessage = ""
+        
+        // Final output initial value
         var realMessage = ""
         
+        // Start of the loop for pieces
         for piece in textPieces {
             
             print(piece)
+            
+            // Declare a string constants for pieces that's used for conditions later on
             let pieceAsString = String(piece)
             
-            
+            // Start of the loop for characters
             for character in piece {
+                
+                // Declare a string constants for characters that's used for conditions later on.
+                let characterAsString = String(character)
+                
+                // If the string has less than 10 characters besides dashes, there's no output.
                 var wordCount = pieceAsString.count
                 if pieceAsString.contains("-") {
                     wordCount -= 1
                 }
                 
-                let characterAsString = String(character)
-                
-                if convertedMessage.count == 3 {
-                    convertedMessage += "-"
-                }
-                else if convertedMessage.count == 7 {
-                    convertedMessage += "-"
-                }
+                // If the first or second branch is true, the rest will not run. The code below is showing the actual converting process.
                 if wordCount < 10 {
                     convertedMessage += ""
                 }
@@ -128,30 +135,37 @@ class ViewController: UIViewController {
                 else if characterAsString == "0" {
                     convertedMessage += "0"
                 }
-                    
+                  // If it's something other than numbers and letters, ignore.
                 else {
                     convertedMessage += ""
                 }
-                
-                
+                // When the output has 3 characters, add a dash.
+                if convertedMessage.count == 3 {
+                    convertedMessage += "-"
+                }
+                // When the output has 7 characters, add a dash.
+                else if convertedMessage.count == 7 {
+                    convertedMessage += "-"
+                }
             }
-            
+            // If the final message is less than 12 digits, it's not valid. If it's empty, it will not skip to the next line.
             if convertedMessage == "" || convertedMessage.count < 12 {
-             realMessage += ""
-         } else {
-              realMessage += convertedMessage + "\n"
-         }
+                realMessage += ""
+            } else {
+                realMessage += convertedMessage + "\n"
+            }
+            // Reset the value because it will run 3 times.
+            convertedMessage = ""
+            
+        }
         
-         convertedMessage = ""
+        // Final Output
+        // RealMessage is outside of the loop therefore it can collect each output and combine them.
+        outputText.text = realMessage
+        
         
     }
     
     
-        outputText.text = realMessage
     
-    
-}
-
-
-
 }
